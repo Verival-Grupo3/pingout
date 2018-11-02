277961c87b6a8de8f93d4d6f6fffc4589085450f
Victor Gomide <victoragomide@gmail.com>
2018-11-02 15:49:43 -0300

    Fixing utils.py methods tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,13 +11,11 @@ def test_return_200_on_root(client):
     response = client.get('/')
     assert response.status_code == 200
 
-# Test route create-pingout for a post
 def test_return_201_on_post_create_pingout(client):
     response = client.post('create-pingout')
     assert response.status_code == 201
     assert response.data != ''
 
-# Test route create-pingout for a get
 def test_return_405_on_get_create_pingout(client):
     response = client.get('create-pingout')
     assert response.status_code == 405
