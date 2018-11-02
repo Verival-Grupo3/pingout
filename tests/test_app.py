24b3a08e3379277eb7f584b88f2c255dbf4c3a6e
Arthur Assis <arthur120496@gmail.com>
2018-11-02 15:32:03 -0300

    test ping post with invalid uuid

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,10 @@
+import pytest
+
+@pytest.fixture
+def create_pingout(client):
+    response = client.post('create-pingout')
+    uuid = response.json['uuid']
+    return uuid
 
 def test_return_200_on_root(client):
     """ Return status code 200 on root url"""
@@ -16,12 +23,16 @@ def test_return_405_on_get_create_pingout(client):
     assert response.status_code == 405
 
 def test_create_ping_with_valid_UUID(client):
-    response = client.post('create-pingout')
-    uuid = response.json['uuid']
+    uuid = create_pingout(client)
     response = client.post(uuid + '/ping')
     assert response.status_code == 201
 
 def test_create_ping_with_nonexistent_UUID(client):
-    uuid = "de21ff11e30047ae9698f6bed2529ca4"
+    uuid = 'de21ff11e30047ae9698f6bed2529ca4'
     response = client.post(uuid + '/ping')
     assert response.status_code == 404
+
+def test_create_ping_with_invalid_UUID(client):
+    uuid = 'de21ff11e30047ae9698f6bed2529caj'
+    response = client.post(uuid + '/ping')
+    assert response.status_code == 400
